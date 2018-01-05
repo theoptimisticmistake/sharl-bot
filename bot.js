0f3edf7d511b7fa9749fa2cd4eadbeff1ac93c86
theoptimisticmistake <35154023+theoptimisticmistake@users.noreply.github.com>
2018-01-05 23:19:39 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  } else
+
+  if (message.content.startsWith("gay")) {
+      message.channel.send("yay!");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);  
